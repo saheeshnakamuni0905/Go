146ff991b8539ed51e9ce339b6454dd9a38a2df8
saheeshnakamuni0905 <kamunisaheeshna5@gmail.com>
2024-06-17 05:41:51 -0400

    MathematicalExpressions

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 23, 45, 89
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer Sum ", intSum)
+
+	f1, f2, f3 := 23.99, 45.777, 89.909
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum ", floatSum)
+
+	floatSum = math.Round(floatSum)
+	fmt.Println("The sum is now", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100 //safest way to round digits
+	fmt.Println("The sum is now", floatSum)
+
+	circleRadius := 5.9
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circumference: %.2f\n", circumference)
 
 }
